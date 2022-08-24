ec3aeb81b12fdfe8707814f5da1885975d5ea3a9
lebington <Leban.abe@gmail.com>
2022-08-24 13:56:41 -0500

    able to interact with website and enter in values

--- testing.py
+++ testing.py
@@ -1,16 +1,28 @@
-# tutorial for selenium : "https://www.youtube.com/watch?v=b5jt2bhSeXs&ab_channel=TechWithTim"
+#Automating Test clone 
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by   import By
+
 
 PATH = "D:\chromedriver.exe "
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.google.com/")
+driver.get('https://www.youtube.com/');
 
 
 
+search_box = driver.find_element("name", "search_query")
 
-driver.close()
+search_box.send_keys('ChromeDriver')
 
+search_box.submit()
+
+
+# MAKE EACH LINE HAVE ITS OWN THING
+# initialize(1334)
+# def initialize(n):
+#     search_box = driver.find_element("name", "CN1")
+#     search_box.send_keys(n)
 

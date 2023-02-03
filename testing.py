9dbeaa7b2aa24ddcd62fc0c7715d94140a9c9c61
lebington <Leban.abe@gmail.com>
2023-02-03 12:33:29 -0600

    updated to newest Chrome Driver

--- testing.py
+++ testing.py
@@ -3,7 +3,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by   import By
+import chromedriver_autoinstaller
 
+chromedriver_autoinstaller.install()
 
 PATH = "D:\chromedriver.exe "
 
